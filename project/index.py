4ed1cb38ed4675812bf7631b0cf7b7b26ca2fbca
Mohith Pranav <pranavmohith@gmail.com>
2025-12-08 01:31:59 +0530

    implemeted user move based on the dice

--- project/index.py
+++ project/index.py
@@ -9,3 +9,16 @@ print(f"Game started. Player is at position: {player_position}")
 
 dice = random.randint(1, 6)
 print(f"player rolled and dice and got : {dice}")
+
+option = random.randint(0, 2)
+
+if option == 0:
+    print("Option 1: No Play")
+elif option == 1:
+    print("Option: Ladder goes forward")
+    player_position += dice
+elif option ==2:
+    print("Option : Sanke bitten and moves back")
+    player_position -= dice
+    
+print(f"Player current position: {player_position}")
