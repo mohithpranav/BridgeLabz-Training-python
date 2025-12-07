fa71a0ad823737b45eacb8a91e354f04e61243d7
Mohith Pranav <pranavmohith@gmail.com>
2025-12-08 01:24:14 +0530

    getting random val from 1 to 6

--- project/index.py
+++ project/index.py
@@ -1,7 +1,11 @@
 print("=== Snake & Ladder Game ===")
 
+import random
+
 START_POSITION = 0
 player_position = START_POSITION
 
-
 print(f"Game started. Player is at position: {player_position}")
+
+dice = random.randint(1, 6)
+print(f"player rolled and dice and got : {dice}")
