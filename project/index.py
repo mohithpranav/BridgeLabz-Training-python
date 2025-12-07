a8e7dd3bff8f40172f114b4eec07fa40d2661d18
Mohith Pranav <pranavmohith@gmail.com>
2025-12-08 02:09:47 +0530

    dice rolled until reaching 100 and count of number of rolls

--- project/index.py
+++ project/index.py
@@ -5,30 +5,35 @@ import random
 START_POSITION = 0
 WINNING_POSITION = 100
 player_position = START_POSITION
+dice_cnt = 0
 
 print(f"Game started. Player is at position: {player_position}")
 
-dice = random.randint(1, 6)
-print(f"player rolled and dice and got : {dice}")
+while player_position != WINNING_POSITION:
+    dice = random.randint(1, 6)
+    dice_cnt += 1
+    print(f"player rolled and dice and got : {dice}")
 
-option = random.randint(0, 2)
+    option = random.randint(0, 2)
 
-if option == 0:
-    print("Option 1: No Play")
-    
-elif option == 1:
-    print("Option: Ladder goes forward")
-    new_pos = player_position + dice
-    if new_pos > WINNING_POSITION:
-        print("Move goes beyond 100. cant move froward")
-    else:
-        player_position = new_pos  
-          
-elif option ==2:
-    print("Option : Sanke bitten and moves back")
-    player_position -= dice
-    
-if player_position < 0: 
-    player_position = 0
-    
-print(f"Player current position: {player_position}")
+    if option == 0:
+        print("Option 1: No Play")
+        
+    elif option == 1:
+        print("Option: Ladder goes forward")
+        new_pos = player_position + dice
+        if new_pos > WINNING_POSITION:
+            print("Move goes beyond 100. cant move froward")
+        else:
+            player_position = new_pos  
+            
+    elif option ==2:
+        print("Option : Sanke bitten and moves back")
+        player_position -= dice
+        
+    if player_position < 0: 
+        player_position = 0
+        
+    print(f"Player current position: {player_position}")
+
+print(f"Player reached {player_position}. \nTotal dice rolled to win the game: {dice_cnt} ")
